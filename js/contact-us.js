0276d6059df2f43b41cbc988a879c5aa3e0f3dd8
Nguyen Danh Hieu <nguyendanhhieu.0602@gmail.com>
2025-07-03 16:25:10 +0700

    first commit

---
+++ js/contact-us.js
@@ -0,0 +1,13 @@
+function validationForm() {
+  let information = document.forms["informationAccount"];
+  let firstName = information["firstName"].value;
+  let lastName = information["lastName"].value;
+  let email = information["email"].value;
+  let phone = information["phone"].value;
+
+  if(firstName=="" || lastName=="" || email=="" || phone=="") {
+    alert("Please fill out all the information with *!");
+    return false;
+  }
+} 
+
